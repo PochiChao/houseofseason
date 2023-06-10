dba249e050e7c181d5cb7dd7758622b9537dbf97
Pochi Chao <19pochi94@gmail.com>
2023-06-10 09:33:19 -0500

    correct season logic

--- src/components/restaurantTitle.tsx
+++ src/components/restaurantTitle.tsx
@@ -1,16 +1,16 @@
-function RestaurantTitle(){
-    const currentMonth = new Date().getMonth();
-    let currentSeason = "";
-    if (currentMonth === 0 || currentMonth === 1  || currentMonth === 11){
-        currentSeason = "Winter";
-    } else if (currentMonth < 6){
-        currentSeason = "Spring";
-    } else if (currentMonth < 9){
-        currentSeason = "Summer";
-    } else {
-        currentSeason = "Autumn";
-    }
-    const restaurantTitle = `House of ${currentSeason}`;
-    return restaurantTitle;
+function RestaurantTitle() {
+  const currentMonth = new Date().getMonth();
+  let currentSeason = "";
+  if (currentMonth === 0 || currentMonth === 1 || currentMonth === 11) {
+    currentSeason = "Winter";
+  } else if (currentMonth < 5) {
+    currentSeason = "Spring";
+  } else if (currentMonth < 8) {
+    currentSeason = "Summer";
+  } else {
+    currentSeason = "Autumn";
+  }
+  const restaurantTitle = `House of ${currentSeason}`;
+  return restaurantTitle;
 }
 export default RestaurantTitle;
